331b5ef94b32c45c148219784374f98f4a3fc3ff
Marco Roth <marco.roth@intergga.ch>
2024-12-08 21:13:07 +0100

    Initial commit

---
+++ RubyEventsTests/RubyEventsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RubyEventsTests.swift
+//  RubyEventsTests
+//
+//  Created by Marco Roth on 08.12.2024.
+//
+
+import Testing
+@testable import RubyEvents
+
+struct RubyEventsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
