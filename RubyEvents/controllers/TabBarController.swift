45fcf0e7b1419f446db81f1fb2658cbe25ee0bf6
Leon <nonick@nonick.ch>
2025-03-30 20:25:46 +0200

    Add basic TabBar styling

--- RubyEvents/controllers/TabBarController.swift
+++ RubyEvents/controllers/TabBarController.swift
@@ -134,16 +134,26 @@ class TabBarController: UITabBarController {
       }
     }
 
+    // NavBar styling
     let navigationBarAppearance = UINavigationBarAppearance()
     navigationBarAppearance.configureWithDefaultBackground()
-
     UINavigationBar.appearance().standardAppearance = navigationBarAppearance
     UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
     UINavigationBar.appearance().isOpaque = false
     UINavigationBar.appearance().isTranslucent = true
-
     navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
 
+    // TabBar styling
+    let tabBarAppearance = UITabBarAppearance()
+    tabBarAppearance.configureWithDefaultBackground()
+    tabBarAppearance.backgroundColor = UIColor.white
+    UITabBar.appearance().standardAppearance = tabBarAppearance
+    UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+    UITabBar.appearance().isOpaque = true
+    UITabBar.appearance().isTranslucent = false
+    UITabBar.appearance().tintColor = .red
+    UITabBar.appearance().unselectedItemTintColor = .black
+      
     self.isSetup = true
   }
 
