a3a2753bf7f62a683e39393ec3c9732dbf8ec3de
Marco Roth <marco.roth@intergga.ch>
2025-04-01 17:05:02 -0300

    Use RubyEvents.org endpoint

--- RubyEvents/lib/Endpoint.swift
+++ RubyEvents/lib/Endpoint.swift
@@ -2,8 +2,8 @@ import Foundation
 
 struct Endpoint {
   static let development = URL(string: "http://172.20.4.17:3000")!
-  static let staging = URL(string: "https://www.rubyvideo.dev")!
-  static let production = URL(string: "https://www.rubyvideo.dev")!
+  static let staging = URL(string: "https://www.rubyevents.org")!
+  static let production = URL(string: "https://www.rubyevents.org")!
 
   static func url(environment: Environment = .development) -> URL {
     switch environment {
